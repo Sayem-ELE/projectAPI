76e51880ad9d803b06ee3d320e6997a26e9559c2
Sayem-ELE <sayem.libra@gmail.com>
2025-07-17 19:23:07 +0600

    dom manipulation added

--- js/store.js
+++ js/store.js
@@ -6,10 +6,39 @@ function handleSearch(){
 
 const loadPhone = async (searchText) => {
     const res = await fetch(
-        "https://openapi.programming-hero.com/api/phones?search=samsung"
+        `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     );
 
     console.log("Server Response: ", res);
-    const data = await res.json();
+    const searverData = await res.json();
+
+    displayPhone(searverData.data);
+};
+
+const displayPhone = (data) => {
     console.log(data);
-}
+    const cardContainer = document.getElementById("card-section");
+
+    cardContainer.innerHTML = "";
+    data.forEach((phone) => {
+        const productCard = document.createElement("div");
+        productCard.classList.add("card");
+
+        productCard.innerHTML = `
+         <div class="card-image">
+                    <img src=${phone.image} alt="phone-image">
+                </div>
+                <h3 class="card-title">${phone.phone_name}</h3>
+                <p class="card-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nihil.</p>
+                <div class="card-price">
+                    <span>$</span>
+                    <span id="card-item-price">999</span>
+                </div>
+                <div class="card-button">
+                    <button class="btn">Show Details</button>
+                </div>
+        `; 
+        
+        cardContainer.appendChild(productCard);
+    });
+};
